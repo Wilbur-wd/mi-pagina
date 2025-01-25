15f2ce6a9e966a8b8057b7a4d23d8c98c6324399
Wilbur-wd <leodomz23@gmail.com>
2025-01-25 00:44:02 -0600

    Add files via upload

---
+++ Pag/js/site.js
@@ -0,0 +1,5 @@
+﻿// Función para alternar la clase "open" en el menú
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    const menu = document.querySelector('.icons'); // Seleccionar el menú
+    menu.classList.toggle('open'); // Alternar la clase "open"
+});
