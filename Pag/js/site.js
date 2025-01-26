d8d40abddf32513e4b390961a8d19ca46397a275
Wilbur-wd <leodomz23@gmail.com>
2025-01-26 17:27:40 -0600

    Delete Pag directory

--- Pag/js/site.js
+++
@@ -1,5 +0,0 @@
-﻿// Función para alternar la clase "open" en el menú
-document.querySelector('.menu-toggle').addEventListener('click', function () {
-    const menu = document.querySelector('.icons'); // Seleccionar el menú
-    menu.classList.toggle('open'); // Alternar la clase "open"
-});
